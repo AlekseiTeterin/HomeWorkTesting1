3de9678b38bd735b4cba8b5a4fb6d200f17e3da2
Aleksei Teterin <alexey_teterin@mail.ru>
2022-10-18 15:06:44 +0300

    Initial commit

---
+++ src/Figures.java
@@ -0,0 +1,15 @@
+public class Figures {
+
+    public double AreaOfTriangle(double base, double high){
+        return 0.5 * base * high;
+    }
+    public double AreaOfRectangle(double length, double width){
+        return length * width;
+    }
+    public double AreaOfSquare(double length){
+        return length * length;
+    }
+    public double AreaOfRhombus(double length, double high){
+        return length * high;
+    }
+}
